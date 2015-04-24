8be537c74697686013baa1da8da397243a8b9848
Afa.L Cheng <afa@afa.moe>
2015-04-23 22:19:07 -0500

    init

---
+++ png2b64/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace png2b64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                throw new Exception("Wrong args");
+            }
+            string b64 = ToB64(args[0]);
+
+            if (args.Length > 1)
+            {
+                StreamWriter file = new StreamWriter(args[1]);
+                file.WriteLine(b64);
+                file.Close();
+            }
+            else
+            {
+                Console.Out.WriteLine(b64);
+            }
+        }
+        
+
+        static public string ToB64(string path)
+        {
+            return Convert.ToBase64String(File.ReadAllBytes(path));
+        }
+
+    }
+}
